42cca24d12c809003ba20f82137dd7595f39a9fe
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-12-28 02:17:47 -0300

    salvation.pyd-salvation.pym-salvation.pyY salvation.pyH:salvation.pyM - Arch Linux

---
+++ salvation.py
@@ -0,0 +1,25 @@
+import json
+import random
+
+with open("biblia.json", "r", encoding="utf-8-sig") as file:
+    biblia = json.load(file)
+
+def escolher_palavra():
+    livro = random.choice(biblia)
+    nome_livro = livro["abbrev"]
+
+    capitulo_idx = random.randint(0, len(livro["chapters"]) - 1)
+    capitulo = livro["chapters"][capitulo_idx]
+
+    if len(capitulo) >= 3:
+        versiculo_idx = random.randint(0, len(capitulo) - 3)
+    else:
+        versiuclo_idx = 0 # se pegar um com mneos de 3
+
+    trecho = capitulo[versiculo_idx:versiculo_idx + 3]
+
+    print(f"\nLivro: {nome_livro}, Capitulo: {capitulo_idx + 1}, Versículos {versiculo_idx + 1} - {versiculo_idx + 3}")
+    for i, texto in enumerate(trecho, start=1):
+        print(f"{versiculo_idx + i}: {texto}")
+
+escolher_palavra()
