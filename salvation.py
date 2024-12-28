4b5c675248831973e0adc18b8c2d0db7390583ef
Gabrieel-Lopees <gabriel-lopes-universidade@proton.me>
2024-12-28 02:27:34 -0300

    salvation.pyd-salvation.pym-salvation.pyY salvation.pyH:salvation.pyM - Arch Linux

--- salvation.py
+++ salvation.py
@@ -1,5 +1,10 @@
 import json
 import random
+import os
+
+
+# aqui começa a salvação!! =D
+
 
 with open("biblia.json", "r", encoding="utf-8-sig") as file:
     biblia = json.load(file)
@@ -7,7 +12,6 @@ with open("biblia.json", "r", encoding="utf-8-sig") as file:
 def escolher_palavra():
     livro = random.choice(biblia)
     nome_livro = livro["abbrev"]
-
     capitulo_idx = random.randint(0, len(livro["chapters"]) - 1)
     capitulo = livro["chapters"][capitulo_idx]
 
@@ -16,10 +20,25 @@ def escolher_palavra():
     else:
         versiuclo_idx = 0 # se pegar um com mneos de 3
 
+
+
     trecho = capitulo[versiculo_idx:versiculo_idx + 3]
-
-    print(f"\nLivro: {nome_livro}, Capitulo: {capitulo_idx + 1}, Versículos {versiculo_idx + 1} - {versiculo_idx + 3}")
+    print(f"\nTrecho da Bíblia:")
+    print(f"Livro: {nome_livro}, Capítulo: {capitulo_idx + 1}, Versículos {versiculo_idx + 1} - {versiculo_idx + 3}")
     for i, texto in enumerate(trecho, start=1):
-        print(f"{versiculo_idx + i}: {texto}")
+       print(f"{versiculo_idx + i}: {texto}")
 
-escolher_palavra()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#escolher_palavra() // função abandonada do arquivo .py
